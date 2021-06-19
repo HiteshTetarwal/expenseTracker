7ea1903b28ceda750c5b3ef9bfabc765ea2d8454
Hitesh Kumar Tetarwal <47805800+HiteshTetarwal@users.noreply.github.com>
2021-06-20 00:35:44 +0530

    expense tracker initialisation

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //using one state instead of multiple states
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (event) => {
+    //using separate state
+    setEnteredTitle(event.target.value);
+
+    //bad practive to use single state
+    // setUserInput({
+    //     ...userInput,
+    //     eneteredTitle: event.target.value
+    // });
+
+    //good way to use single state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    //using separate state
+    setEnteredDate(event.target.value);
+
+    //bad practive to use single state
+    // setUserInput({
+    //   ...userInput,
+    //   eneteredDate: event.target.value,
+    // });
+
+    //good way to use single state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    //using separate state
+    setEnteredAmount(event.target.value);
+
+    //bad practive to use single state
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+
+    //good way to use single state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.PreventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    //reset the form
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            step="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
